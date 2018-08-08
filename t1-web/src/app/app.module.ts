afc24fd804131fdce6f3327b6f249ae7a509f40b
element <iosthebset@gmail.com>
2018-08-08 10:30:36 +0300

    add header component

--- t1-web/src/app/app.module.ts
+++ t1-web/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
