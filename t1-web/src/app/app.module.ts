e89f90c12cbd4e7f3e768f338939caf1f7c8b8da
element <iosthebset@gmail.com>
2018-08-08 10:02:02 +0300

    add new footer

--- t1-web/src/app/app.module.ts
+++ t1-web/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
